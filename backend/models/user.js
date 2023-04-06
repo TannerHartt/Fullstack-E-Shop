b52b64c46b7223e0e8259742c9d38b750a554a98
Tanner Hart <tannerhart24@yahoo.com>
2023-04-06 11:25:56 -0700

    Further improved user model to have basic validation

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,16 +1,46 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name: String,
-    email: String,
-    passwordHash: String,
-    street: String,
-    apartment: String,
-    city: String,
-    zip: String,
-    country: String,
-    phone: Number,
-    isAdmin: Boolean
+    name: {
+        type: String, 
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    street: {
+        type: String,
+        default: ''
+    },
+    apartment: {
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        default: ''
+    },
+    zip: {
+        type: String,
+        default: ''
+    },
+    country: {
+        type: String,
+        default: ''
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean, 
+        default: false
+    }
 });
 
 userSchema.virtual('id').get(function () {
