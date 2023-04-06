0e854f2fabed4e68a51877885806ef00a786fe4a
Tanner Hart <tannerhart24@yahoo.com>
2023-04-06 10:40:20 -0700

    Mongoose schema to describe a product object

---
+++ backend/models/product.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+
+const productScehma = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    richDescription: {
+        type: String,
+        default: ''
+    },
+    image: {
+        type: String,
+        default: ''
+    },
+    images: [{
+        type: String
+    }],
+    brand: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId, // This is a reference to the Category model
+        ref: 'Category',
+        required: true
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    numReviews: {
+        type: Number,
+        default: 0
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Product = mongoose.model('Product', productScehma);
+
+module.exports = Product;
