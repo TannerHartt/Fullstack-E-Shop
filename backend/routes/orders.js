a338248f53b0ec6566089b3c1e78448ff217fe80
Tanner Hart <tannerhart24@yahoo.com>
2023-04-24 14:56:50 -0700

    Created get all orders, get order, and create order routes and populated any references

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,14 +1,78 @@
 const express = require('express');
 const router = express.Router();
 const Order = require('../models/orders');
+const OrderItem = require('../models/order-item');
 
 router.get('/', async (req, res) => {
-    const orders = await Order.find();
+    // Include user information in the response and sort the orders oldest to newest
+    const orders = await Order.find().populate('user', 'name email').sort({ 'dateOrdered': -1 });
 
     if (!orders) {
         res.status(500).json({ success: false })
     }
-    res.send(orders);
+    res.status(200).json({
+        success: true,
+        count: orders.length,
+        orders
+    });
+});
+
+router.get('/:id', async (req, res) => {
+    // Include user information in the response and sort the orders oldest to newest
+    const order = await Order.findById(req.params.id)
+        .populate('user', 'name email')
+        .populate({ 
+            path: 'orderItems', populate: { 
+                path: 'product', populate: 'category' 
+            }
+        });
+
+    if (!order) {
+        res.status(500).json({ success: false })
+    }
+    res.status(200).json({  
+        success: true,
+        order
+    });
+});
+
+router.post('/', async (req, res) => {
+
+    const orderItemsIds = Promise.all(req.body.orderItems.map(async (orderItem) => {
+        let newOrderItem = new OrderItem({
+            quantity: orderItem.quantity,
+            product: orderItem.product
+        });
+
+        newOrderItem = await newOrderItem.save();
+
+        return newOrderItem._id;
+    }));
+
+    const orderItemsIdsResolved = await orderItemsIds;
+
+    let order = new Order({
+        orderItems: orderItemsIdsResolved,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: req.body.totalPrice,
+        user: req.body.user,
+        dateOrdered: req.body.dateOrdered
+    });
+
+    order = await order.save();
+
+    if (!order) return res.status(404).send('The order cannot be created!');
+
+    res.status(200).json({
+        success: true,
+        order,        
+    });
 });
 
 module.exports = router;
